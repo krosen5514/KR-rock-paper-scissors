bc8c9e7fb07647daf80e4c07283d7b97d46287c0
krosen5514 <84346108+krosen5514@users.noreply.github.com>
2021-05-25 21:19:38 -0400

    Added winning scenarios, updated design

--- game.py
+++ game.py
@@ -2,11 +2,13 @@
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!")
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
 
-player_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
+player_choice = input("Please choose eith: 'rock', 'paper', 'scissors': ")
 
-print("The player chose:", player_choice)
+print("You chose:", player_choice)
 
 #validate the input such that only if it is one of the expected values
 #...will we continue with the rest of the program
@@ -18,10 +20,38 @@ if (player_choice =="rock") or (player_choice =="paper") or (player_choice =="sc
 else: 
     print("OOPS, Invalid input. Please try again.")
     exit()
-
+print("-------------------")
 
 valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
 print("The computer chose:", computer_choice)
 
-print("This is the end of our game. Please play again.")
+#Rock beats Scissors
+#Paper beats Rock
+#Scissors beats Paper
+#Rock vs Rock, Paper vs Paper, and Scissors vs Scissors each results in a "tie"
+print("-------------------")
+if (player_choice =="rock") and (computer_choice == "scissors"):
+    print("Computer Wins")
+if (player_choice =="scissors") and (computer_choice == "rock"):
+    print("Computer Wins")
+if (player_choice =="rock") and (computer_choice == "paper"):
+    print("Computer Wins")
+
+if (computer_choice =="rock") and (player_choice == "scissors"):
+    print("You Win!")
+if (computer_choice =="scissors") and (player_choice == "rock"):
+    print("You Win!")
+if (computer_choice =="rock") and (player_choice == "paper"):
+    print("You Win!")
+
+if (computer_choice =="rock") and (player_choice == "rock"):
+    print("It's a tie")
+if (computer_choice =="scissors") and (player_choice == "scissors"):
+    print("It's a tie")
+if (computer_choice =="paper") and (player_choice == "paper"):
+    print("It's a tie")
+
+print("-------------------")
+
+print("Thanks for playing. Please play again.")
